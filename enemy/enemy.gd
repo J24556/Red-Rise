1bf7bcdb1f4acc75f036a82250dd2091c9b4866a
j124 <47467360+j124UMBC@users.noreply.github.com>
2021-02-18 12:50:42 -0500

    enemy vision+shooting

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -2,6 +2,9 @@ class_name Enemy
 extends RigidBody2D
 
 const WALK_SPEED = 50
+const SHOT_SPEED = 15
+const VISION_LENGTH = 100
+var SHOT_DELAY = 0.3
 
 enum State {
 	WALKING,
@@ -13,10 +16,12 @@ var state = State.WALKING
 var direction = -1
 var anim = ""
 
-var Bullet = preload("res://player/player_bullet.gd")
-
+var Bullet_gd = preload("res://player/player_bullet.gd")
+var Bullet = preload("res://player/PlayerBullet.tscn") #change to player damaging tscn
+onready var shot_delay_timer = $ShotDelay
 onready var rc_left = $RaycastLeft
 onready var rc_right = $RaycastRight
+onready var rc_shoot = $RaycastGun
 
 func _integrate_forces(s):
 	var lv = s.get_linear_velocity()
@@ -34,7 +39,7 @@ func _integrate_forces(s):
 			var dp = s.get_contact_local_normal(i)
 
 			if cc:
-				if cc is Bullet and not cc.disabled:
+				if cc is Bullet_gd and not cc.disabled:
 					# enqueue call
 					call_deferred("_bullet_collider", cc, s, dp)
 					break
@@ -43,6 +48,14 @@ func _integrate_forces(s):
 				wall_side = 1.0
 			elif dp.x < -0.9:
 				wall_side = -1.0
+
+		if rc_shoot.is_colliding() and shot_delay_timer.is_stopped():
+			shot_delay_timer.start(SHOT_DELAY)
+			
+			var bi = Bullet.instance()
+			bi.global_position = rc_shoot.global_position
+			get_parent().add_child(bi)
+			bi.set_velocity(rc_shoot.get_cast_to() * -SHOT_SPEED)
 
 		if wall_side != 0 and wall_side != direction:
 			direction = -direction
@@ -53,7 +66,8 @@ func _integrate_forces(s):
 		elif direction > 0 and not rc_right.is_colliding() and rc_left.is_colliding():
 			direction = -direction
 			($Sprite as Sprite).scale.x = direction
-
+ 
+		rc_shoot.set_cast_to(Vector2(direction*-VISION_LENGTH,0))
 		lv.x = direction * WALK_SPEED
 
 	if anim != new_anim:
